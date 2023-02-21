9840251599d7ca7d1a6d35c5c7a55b855a8cca11
rickysoro <ricky.soro@gmail.com>
2023-02-21 18:14:20 +0100

    Added InMemoryStorageService

---
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.Core/Exceptions/ReviewNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmEHistoryReviewBackend.Core.Exceptions
+{
+    public class ReviewNotFoundException : Exception
+    {
+        public ReviewNotFoundException(int id) : base($"No review with id {id} was found")
+        {
+        }
+    }
+}
---
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.Core/Service/InMemoryStorageService.cs
@@ -0,0 +1,49 @@
+﻿using FilmEHistoryReviewBackend.Core.Exceptions;
+using FilmEHistoryReviewBackend.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmEHistoryReviewBackend.Core.Service
+{
+    public class InMemoryStorageService : IStorageService
+    {
+        public List<Review> _reviews;
+
+        public InMemoryStorageService()
+        {
+            _reviews = new List<Review>();
+        }
+
+        public void DeleteReview(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Review FindReviewByIdOrFail(int id) 
+        {
+            var reviewToFind = _reviews.FirstOrDefault(review => review.Id == id);
+            if (reviewToFind == null) throw new ReviewNotFoundException(id);
+            return reviewToFind;
+        }
+
+        public List<Review> GetAllReviews() => _reviews;
+
+        public Review GetReviewById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Review InsertReview(int userId, int movieId, string comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Review UpdateReview(int id, string comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
