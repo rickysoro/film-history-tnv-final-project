bd334ad9139045d84142d0d0134c7e9121832e48
rickysoro <ricky.soro@gmail.com>
2023-02-21 17:08:20 +0100

    Added Interface

--- .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.Core/Models/Review.cs
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.Core/Models/Review.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FilmEHistoryReviewBackend.Core.Models
 {
-    internal class Review
+    public class Review
     {
+        private int _id;
+        private int _user_id;
+        private int _movie_id;
+        private string _comment;
+
+        public Review(int id, int user_id, int movie_id, string comment) 
+        {
+            _id = id;
+            _user_id = user_id;
+            _movie_id = movie_id;
+            _comment = comment;
+        }
+
+        public int Id 
+        { 
+            get { return _id; } 
+        }
+
+        public int UserId
+        { 
+            get { return _user_id; } 
+            set { _user_id = value; }
+        }
+
+        public int MovieId
+        { 
+            get { return _movie_id; } 
+            set { _movie_id = value; } 
+        }
+
+        public string Comment
+        {
+            get { return _comment; } 
+            set { _comment = value; } 
+        }
     }
 }
---
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.Core/Service/IStorageService.cs
@@ -0,0 +1,22 @@
+﻿using FilmEHistoryReviewBackend.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmEHistoryReviewBackend.Core.Service
+{
+    public interface IStorageService
+    {
+        public Review InsertReview(int userId, int movieId, string comment);
+
+        public void DeleteReview(int id);
+
+        public List<Review> GetAllReviews();
+
+        public Review GetReviewById(int id);
+
+        public Review UpdateReview(int id, string comment); 
+    }
+}
