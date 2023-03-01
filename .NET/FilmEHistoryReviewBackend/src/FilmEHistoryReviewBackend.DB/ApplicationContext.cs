2e2997c9fdc80e88f0ac922080dc3676e7cfbb52
rickysoro <ricky.soro@gmail.com>
2023-03-01 12:08:37 +0100

    Added Model and Mapper

--- .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.DB/ApplicationContext.cs
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.DB/ApplicationContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace FilmEHistoryReviewBackend.DB
 {
-    internal class ApplicationContext
+    public class ApplicationContext : DbContext
     {
+        public ApplicationContext() 
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var version = new MySqlServerVersion(new Version(8, 0, 30));
+            var connectionString = "Server = localhost; Port = 3306; Database = film_history_db; Uid = root; Pwd = Rt28p4";
+
+            optionsBuilder.UseMySql(connectionString, version);
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
---
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.DB/Mapper/ReviewEntityMapper.cs
@@ -0,0 +1,18 @@
+﻿using FilmEHistoryReviewBackend.Core.Models;
+using FilmEHistoryReviewBackend.DB.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmEHistoryReviewBackend.DB.Mapper
+{
+    public class ReviewEntityMapper
+    {
+        public static Review From(ReviewEntity entity)
+        {
+            return new Review(entity.Id, entity.UserId, entity.MovieId, entity.Comment);
+        }
+    }
+}
