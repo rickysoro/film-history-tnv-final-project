ebf551e2a8fa782f926905ca54a27bd5a9b9583e
rickysoro <ricky.soro@gmail.com>
2023-03-13 18:19:27 +0100

    Initialized MySqlStorageService

--- .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.DB/MySqlStorageService.cs
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.DB/MySqlStorageService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FilmEHistoryReviewBackend.Core.Models;
+using FilmEHistoryReviewBackend.Core.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,39 @@ using System.Threading.Tasks;
 
 namespace FilmEHistoryReviewBackend.DB
 {
-    internal class MySqlStorageService
+    public class MySqlStorageService : IStorageService
     {
+        ApplicationContext _context;
+
+        public MySqlStorageService()
+        {
+            _context = new ApplicationContext();
+            _context.Database.EnsureCreated();
+        }
+
+        public void DeleteReview(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Review> GetAllReviews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Review GetReviewById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Review InsertReview(int userId, int movieId, string comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Review UpdateReview(int id, string comment)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
