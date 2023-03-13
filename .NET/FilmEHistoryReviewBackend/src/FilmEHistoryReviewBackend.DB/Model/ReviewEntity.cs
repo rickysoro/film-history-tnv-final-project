c64b04127e084c973330ec1f2a3e64d64d91547d
rickysoro <ricky.soro@gmail.com>
2023-03-13 14:25:39 +0100

    Modified Entity Name

--- .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.DB/Model/ReviewEntity.cs
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.DB/Model/ReviewEntity.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace FilmEHistoryReviewBackend.DB.Model
 {
-    [Table("review")]
+    [Table("review_backend")]
     public class ReviewEntity
     {
         [Column("id"), Key]
