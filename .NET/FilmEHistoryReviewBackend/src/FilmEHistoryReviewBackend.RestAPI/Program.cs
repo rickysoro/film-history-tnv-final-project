fbebafc5d6599b305d0bd23dbe7823039154d7cf
rickysoro <ricky.soro@gmail.com>
2023-03-14 18:00:01 +0100

    Added Comments To Program
    Added Cors

--- .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.RestAPI/Program.cs
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.RestAPI/Program.cs
@@ -13,12 +13,16 @@ namespace FilmEHistoryReviewBackend.RestAPI
 
             // Add services to the container.
 
+            // Aggiungo l'abilitazione ai Cors per il mio programma
+            builder.Services.AddCors();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            /*** Aggiungo un Singleton per il quale indico che possiedo un'interfaccia che dichiara i metodi di storing (IStorageService) e che la classe che 
+             * implementa questi metodi è MySqlStorageService ***/
             builder.Services.AddSingleton<IStorageService, MySqlStorageService>();
+            // Aggiungo un Singleton al quale assegno la classe che mi consente di gestire lo storing attraverso la dependency injection 
             builder.Services.AddSingleton<ReviewManager>();
 
             var app = builder.Build();
@@ -29,6 +33,14 @@ namespace FilmEHistoryReviewBackend.RestAPI
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            // Definisco i permessi dei Cors
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
 
             app.UseAuthorization();
 
