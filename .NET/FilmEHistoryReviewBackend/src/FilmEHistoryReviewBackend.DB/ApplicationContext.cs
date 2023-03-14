c9886c6bc3e728bdf966235059e0509fd7b96146
rickysoro <ricky.soro@gmail.com>
2023-03-14 18:07:34 +0100

    Added Comments To ApplicationContext

--- .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.DB/ApplicationContext.cs
+++ .NET/FilmEHistoryReviewBackend/src/FilmEHistoryReviewBackend.DB/ApplicationContext.cs
@@ -8,10 +8,14 @@ using System.Threading.Tasks;
 
 namespace FilmEHistoryReviewBackend.DB
 {
+    /*** Implemento una classe che tenga allineato il contesto dell'applicazione con il database. La classe estende DbContext (che è classe dell'Entity Framework) 
+     * e permette, attraverso le proprietà di DbContext stesso, di interagire con il DB e gestire le migrations ***/
     public class ApplicationContext : DbContext
     {
+        // Dichiaro un DbSet (collezione di dati) di ReviewEntity (la trasposizione a codice della tabella)
         public DbSet<ReviewEntity> Reviews { get; set; }
 
+        // Implemento un metodo OnConfigurinf per la configurazione del DB
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var version = new MySqlServerVersion(new Version(8, 0, 30));
